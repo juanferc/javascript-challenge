b193e19cf0b8477acb80ffe39cb18d30aef9b736
Juan F. Castellanos <juanferc.24@gmail.com>
2020-10-06 15:58:27 -0400

    Added app.js file

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,36 @@
+var table_data = data; 
+
+var table_body = d3.select("tbody");
+
+
+
+table_data.forEach((data) => {
+    var row = table_body.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+button.on("click", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var input_date = d3.select("#datetime");
+    var input_value = input_date.property("value");
+
+    d3.select("tbody").selectAll("tr").remove();
+    var filtered_data = table_data.filter(table_data => table_data.datetime === input_value);
+
+    filtered_data.forEach((date_input) => {
+        var row = table_body.append("tr");
+        Object.entries(date_input).forEach(([key,value]) => {
+            var cell = table_body.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+    
